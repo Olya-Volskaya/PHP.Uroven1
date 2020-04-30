b0a88ac6b5293625ad39b0faf547a22480366096
Olya-Volskaya <olg-vakorina@yandex.ru>
2020-04-30 15:46:30 +0300

    Решение к уроку 5

---
+++ Urok5/config.php
@@ -0,0 +1,8 @@
+<?php
+const SERVER = "localhost";
+const DB = "galereya";
+const LOGIN = "root";
+const PASS = "root";
+
+$connect = mysqli_connect(SERVER, LOGIN, PASS, DB) or die("Ошибка соединения с базой данных");
+?>
